8d8858a6ec65296386a82c165cd520965ae5ff12
Nosa Omorodion <nosa.omorodion@verraki.com>
2019-06-12 21:54:37 +0100

    Added styles and dynamic rendering to header component

---
+++ components/header/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+
+  },
+  viewStyle: {
+    height: 60,
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItem: 'center',
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    elevation: 2,
+    position: 'relative'
+    
+  },
+});
