1c005653710a30e98e01ae7a769f51ee7c3fa50d
Nosa Omorodion <nosa.omorodion@verraki.com>
2019-06-13 09:47:19 +0100

    Added axios for fetching data from API

--- components/albumlist/albumlist.js
+++ components/albumlist/albumlist.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, ListView } from 'react-native';
 import styles from './styles';
+import axios from 'axios';
 
 class AlbumList extends Component{
   componentWillMount(){
-    
+    axios.get('https:localhost:8000/api/')
+      .then(response => console.log(response));
+
   }
   render(){
 
